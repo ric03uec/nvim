c6057945691616ed3de7477f2e4ffb22681ad57d
devashish <devashish@esper.io>
2025-04-09 15:56:15 -0700

    adding python lsp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,6 +1,8 @@
 return {
 	{
 		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		enabled = true,
 		dependencies = {
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-buffer",
@@ -18,23 +20,41 @@ return {
 						luasnip.lsp_expand(args.body)
 					end,
 				},
-				mapping = {
-					-- Use Tab to navigate the list
-					["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-					["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+				mapping = cmp.mapping.preset.insert({
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 
-					-- Use Space to confirm the selected item
-					["<Space>"] = cmp.mapping.confirm({ select = true }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 
-					-- Optional: Use <CR> (Enter) as an alternative to confirm
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
 
-					-- Optional: Scroll docs
-					["<C-d>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-				},
+					-- Optional: make <Space> confirm as well (rare)
+					["<Space>"] = cmp.mapping(function(fallback)
+						if cmp.visible() and cmp.get_selected_entry() then
+							cmp.confirm({ select = true })
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+				}),
 				sources = {
 					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
 					{ name = "buffer" },
 					{ name = "path" },
 				},
