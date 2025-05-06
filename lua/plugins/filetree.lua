1f57c693670853b9eebdb3aa1b2fe806f99c2d95
devashish <devashish@esper.io>
2025-05-06 09:54:47 +0530

    restoring nerdtree panel settings

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -5,7 +5,22 @@ return {
 		version = "*",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
+			local function my_on_attach(bufnr)
+				local api = require("nvim-tree.api")
+
+				local function opts(desc)
+					return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+				end
+
+				-- Custom keymaps
+				vim.keymap.set("n", "v", api.node.open.vertical, opts("Open: Vertical Split"))
+				vim.keymap.set("n", "o", api.node.open.tab, opts("Open: New Tab"))
+				vim.keymap.set("n", "<CR>", api.node.open.edit, opts("Open File"))
+				vim.keymap.set("n", "<C-e>", api.tree.close, opts("Close Tree"))
+			end
+
 			require("nvim-tree").setup({
+				on_attach = my_on_attach,
 				view = {
 					width = 30,
 					side = "left",
@@ -16,9 +31,14 @@ return {
 				filters = {
 					dotfiles = false,
 				},
+				actions = {
+					open_file = {
+						quit_on_open = false,
+					},
+				},
 			})
 
-			-- define mapping inside config block
+			-- Leader shortcut to toggle the tree
 			vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { noremap = true, silent = true })
 		end,
 	},
